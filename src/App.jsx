8d914ce9d2a55e1b751d546fc7b97341d3785e04
helloalx <alexa.31hnn@gmail.com>
2026-06-22 16:04:51 +0800

    Implement 3d Product Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import Navbar  from './component/Navbar.jsx';
 import Hero from './component/Hero.jsx';
+import ProductViewer from './component/ProductViewer.jsx';
+import {gsap} from 'gsap';
+import {ScrollTrigger, SplitText} from 'gsap/all';
+
+
+gsap .registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
     <main>
         <Navbar />
         <Hero />
+        <ProductViewer />
     </main>
     )
 }
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+
+import {create} from 'zustand'
+import clsx from 'clsx';
+
+const useMacbookStore = create((set) => ({
+    color : '#2e2c2e',
+    setColor : (color) =>set ({ color }),
+
+    scale : 0.08,
+    setScale : (scale) =>set ({ scale }),
+
+    reset : () => set( { color : '#2e2c2e', scale : 0.08})
+}));
+
+export default useMacbookStore;
